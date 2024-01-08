2dfc0a34cf09063e97e7e590cc46fe694af35aee
Esther Uzoma Ogbu <ogbuuzoma413@gmail.com>
2024-01-08 15:33:08 +0100

    made changes on contact page

--- src/component/layout/WebLayout.tsx
+++ src/component/layout/WebLayout.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from '../block/Header'
+// import Header from '../block/Header'
 import {Outlet} from "react-router-dom"
 import Footer from '../block/Footer'
 
 const WebLayout:React.FC = () => {
   return (
     <div>
-        <Header/>
+        {/* <Header/> */}
         <Outlet/>
         <Footer/>
     </div>
--- src/component/pages/Contact.tsx
+++ src/component/pages/Contact.tsx
@@ -5,8 +5,8 @@ import { SlLocationPin } from "react-icons/sl";
 import { MdOutlineMail } from "react-icons/md";
 const ContactUs = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-<div className="w-[50%] h-[100%] flex justify-center items-center flex-col flex-wrap mr-[30px]">
+    <div className="w-full h-screen flex justify-between items-center p-[40px]">
+<div className="w-[45%] h-[100%] flex justify-center items-center flex-col flex-wrap ml-[30px]">
 <div className="flex justify-between w-[100%]">
    <div className="pl-[10px] ">
    <div className="text-[45px] text-[#CC3333] mb-[20px]"><IoIosPhonePortrait /></div>
