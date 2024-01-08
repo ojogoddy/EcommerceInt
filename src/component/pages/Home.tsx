42aec061df08006bbb61c4fb28b38f02fd745fa1
bolaji-azeez <travistapenu@gmail.com>
2024-01-08 06:55:29 -0800

    new changes

--- src/component/pages/Home.tsx
+++ src/component/pages/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { MdNavigateNext } from "react-icons/md";
-import Earings from "../../assets/earings.jpg"
+import Earings from "../../assets/earings2.jpg"
 import D1 from "../../assets/f1.jpg"
 import D3 from "../../assets/f3.jpg"
 import D4 from "../../assets/f4.jpg"
+import { Link } from 'react-router-dom';
 
 const Home:React.FC = () => {
   return (
-    <div className='bg-[#f2eaea] text-[black] items-center flex justify-center flex-col w-screen min-h-screen pl-10 pr-10'>
+    <div className='bg-[#ffffff] text-[black] gap-10 items-center flex justify-center flex-col w-screen over pl-10 pr-10'>
         <div className='flex items-center justify-center '>
-            <div className='h-[400px] w-[500px] bg-[#e7e1e1] p-5 flex gap-[30px] flex-col justify-center text-[50px]'>
+            <div className='h-[400px] w-[500px] bg-[#ffffff] p-5 flex gap-[30px] flex-col justify-center text-[50px]'>
                 <div className=''>
                     <h2 className='text-[30px] text-left leading-10'>
                         Mixed Pieces 
@@ -31,21 +32,24 @@ const Home:React.FC = () => {
                 
                    
 
-                <div className='flex '>
-                <h3 className='text-[18px]'>
+                <div className='flex cursor-pointer '>
+                <h3 className='text-[18px] '>
                             EXPLORE NOW
                             
                         </h3>
-                        <div className='text-[black] text-[20px] flex items-center'>
+
+                    
+                      <Link to="cart"><div className='text-[black] text-[20px] flex items-center'>
+                          
                           <MdNavigateNext />
-                        </div>
+                        </div></Link>
 
                 </div>
                
             </div>
             <div className='h-[400px] w-[500px]'>
                 <div className='h-[400px] w-[500px]'>
-                    <img className='w-[100%] h-[100%]'
+                    <img className='w-[100%] h-[100%] object-fit'
                      src={Earings} alt="" />
 
                 </div>
@@ -60,16 +64,18 @@ const Home:React.FC = () => {
               <h2 className='text-[30px]'>Customer's Favorite</h2>
             </div>
 
-            <div className='flex gap-10 items-center justify-around'>
+            <div className='flex gap-10 items-center justify-around cursor-pointer'>
                 <div>
                     <div className='h-[200px] w-[200px]'>
                         <img className='w-[100%] h-[100%]'
                             src={D1} alt="" />
                     </div>
                     <div className='text-center'>
-                    <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
-                    <span className='text-[grey] text-[15px]'>$220.49</span>
-                    </div>
+
+                   <Link to="cart"> <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
+                    
+                    <span className='text-[grey] text-[15px]'>$220.49</span> </Link>
+                    </div> 
                     
                 </div>
                 
@@ -80,8 +86,9 @@ const Home:React.FC = () => {
                             src={D3} alt="" />
                     </div>
                     <div className='text-center'>
-                        <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
-                        <span className='text-[grey] text-[15px]'>$220.49</span>
+
+                        <Link to="cart"><h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
+                        <span className='text-[grey] text-[15px]'>$220.49</span></Link>
                         </div>
                     
                 </div>
@@ -92,8 +99,8 @@ const Home:React.FC = () => {
                             src={Earings} alt="" />
                     </div>
                     <div className='text-center'>
-                        <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
-                        <span className='text-[grey] text-[15px]'>$220.49</span>
+                    <Link to="cart"> <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
+                        <span className='text-[grey] text-[15px]'>$220.49</span></Link>
                         </div>
                     
                 </div>
@@ -105,13 +112,33 @@ const Home:React.FC = () => {
                             src={D4} alt="" />
                     </div>
                     <div className='text-center'>
+                    <Link to="cart">
                         <h3 className='text-[black] text-[15px]'>Rings With Rare Gem</h3>
-                        <span className='text-[grey] text-[15px]'>$220.49</span>
+                        <span className='text-[grey] text-[15px]'>$220.49</span></Link>
                         </div>
                     
                 </div>
             </div>
            
+        </div>
+
+        <div className='w-[90%] h-[200px] bg-[#ebebeb] p-10 flex justify-between items-center'>
+          <div>
+            <h2 className='text-[35px]'>
+              Get Discount 20% Off
+            </h2>
+            <p className='text-[15px] text-[grey]'>Subscribe our newsletter and get discount 20% of</p>
+          </div>
+
+          <div className='flex items-center justify-center bg-white'>
+                <div className='w-[250px] h-[40px] border-2 border-[#efefef] flex items-center gap-5 p-2'>
+          
+                 <input name="Email" type="Email" placeholder='Email Address' />
+
+                </div>
+                <button className='w-[120px] h-[40px] bg-red-600 text-white'>Search</button>
+          </div>
+
         </div>
       
     </div>
