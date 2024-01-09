43765d081d6d909c3e8e1a2a93530512e9bb5031
ojogoddy <goddy5867@gmail.com>
2024-01-09 10:14:08 +0100

    Godwin-cart-responsive

--- src/component/pages/Contact.tsx
+++ src/component/pages/Contact.tsx
@@ -5,31 +5,37 @@ import { SlLocationPin } from "react-icons/sl";
 import { MdOutlineMail } from "react-icons/md";
 const ContactUs = () => {
   return (
-    <div className="w-full h-screen flex justify-between items-center p-[40px] overflow-hidden">
-<div className="w-[45%] h-[100%] flex justify-center items-center flex-col flex-wrap ml-[30px]">
-<div className="flex justify-between w-[100%]">
-   <div className="pl-[10px] ">
-   <div className="text-[45px] text-[#CC3333] mb-[20px]"><IoIosPhonePortrait /></div>
-   <h3 className="text-[15px] font-bold text-black text-left mb-[20px]">Phone</h3>
-    <p className="text-[13px] font-medium text-gray-500 text-left mb-[5px]">(0091) 8547 632521</p>
-    <p className="text-[13px] font-medium text-gray-500 text-left">(084) 965 4788</p>
+    <div className="w-full h-screen sm:h-full flex sm:flex-col-reverse justify-between items-center p-[40px] sm:p-4 overflow-hidden">
+<div className="w-[45%] h-[100%] flex justify-center items-center flex-col flex-wrap ml-[30px] sm:ml-4 sm:w-full">
+<div className="flex justify-between w-[100%] sm:flex-col sm:w-full sm:space-y-10">
+   <div className="pl-[10px] sm:pl-0 sm:flex sm:items-start gap-x-2 ">
+    <div className="text-[45px] text-[#CC3333] mb-[20px]"><IoIosPhonePortrait /></div>
+    <div className="">
+    <h3 className="text-[15px] font-bold text-black text-left mb-[20px] sm:text-[25px] sm:mb-2">Phone</h3>
+      <p className="text-[13px] font-medium text-gray-500 text-left mb-[5px]">(0091) 8547 632521</p>
+      <p className="text-[13px] font-medium text-gray-500 text-left">(084) 965 4788</p>
+    </div>
    </div>
-   <div className="pl-[10px] ">
+   <div className="pl-[10px] sm:pl-0 sm:flex sm:items-start gap-x-2  ">
    <div className="text-[45px] text-[#CC3333] mb-[20px]"><SlLocationPin /></div>
-   <h3 className="text-[15px] font-bold text-black text-left mb-[20px]">Address</h3>
+   <div className="">
+   <h3 className="text-[15px] font-bold text-black text-left mb-[20px] sm:text-[25px] sm:mb-2">Address</h3>
     <p className="text-[13px] font-medium text-gray-500 text-left mb-[5px]">69 Halsey St, New York,</p>
     <p className="text-[13px] font-medium text-gray-500 text-left">Ny 10002, USA</p>
    </div>
-   <div className="pl-[10px] ">
+   </div>
+   <div className="pl-[10px] sm:pl-0 sm:flex sm:items-start gap-x-2">
    <div className="text-[45px] text-[#CC3333] mb-[20px]"><MdOutlineMail /></div>
-   <h3 className="text-[15px] font-bold text-black text-left mb-[20px]">Email</h3>
+   <div className="">
+   <h3 className="text-[15px] font-bold text-black text-left mb-[20px] sm:text-[25px] sm:mb-2">Email</h3>
     <p className="text-[13px] font-medium text-gray-500 text-left mb-[5px]">support@domain.com</p>
     <p className="text-[13px] font-medium text-gray-500 text-left mr-[30px]">hello@domain.com</p>
    </div>
+   </div>
 </div>
-<div className="mt-[100px] w-full">
-    <h1 className="text-left font-light text-[40px] mb-[40px]">Keep In Touch With Us</h1>
-  <div className="flex justify-between items-center">
+<div className="mt-[100px] sm:mt-[50px] w-full">
+    <h1 className="text-left font-light text-[40px] mb-[40px] sm:mb-5">Keep In Touch With Us</h1>
+  <div className="flex justify-between items-center sm:flex-col sm:items-start  sm:space-y-10">
     
   <div className={`flex items-center  border-b-[1px] border-gray-300 bg-transparent outline-none w-[180px] font-light text-[12px] pb-[10px] text-gray-600  
     `} >
@@ -48,7 +54,7 @@ const ContactUs = () => {
     </div>
 
   </div>
-  <div className="text-left mt-[100px]">
+  <div className="text-left mt-[100px] sm:mt-[50px]">
     <textarea className="outline-none border-b-[1px] border-gray-300 w-[300px]" name="" placeholder="Enter Message" ></textarea>
   </div>
 <p className="mt-[50px] text-left capitalize text-[13px] font-extrabold">
@@ -56,7 +62,7 @@ const ContactUs = () => {
 </p>
 </div>
 </div>
-<div  className="w-[45%] h-[100%] ml-[30px]" >
+<div  className="w-[45%] h-[100%] ml-[30px] sm:ml-0 sm:w-full sm:mb-5" >
     <img className="w-[100%] h-[100%] object-cover" src={map} alt="" />
 </div >
     </div>
--- src/component/pages/Home.tsx
+++ src/component/pages/Home.tsx
@@ -10,25 +10,19 @@ const Home:React.FC = () => {
   return (
     <div className='bg-white text-[black] items-center flex justify-center flex-col mx-[9rem] py-[3rem] md:mx-[3rem] sm:mx-[2rem] '>
         <div className='flex items-center justify-center bg-[#f4f4f4]  sm:flex-col-reverse '>
-            <div className='h-[600px] w-[50%] sm:w-full sm:h-[400px] bg-[#f4f4f4] p-5 flex gap-[30px] flex-col justify-center text-[50px]'>
+            <div className='h-[600px] w-[50%] sm:w-full sm:h-full bg-[#f4f4f4] p-5 flex gap-[30px] flex-col justify-center text-[50px]'>
                 <div className=''>
-                    <div className='text-[40px] sm:text-[45px] text-left leading-10 font-semibold sm:text-center'>
+                    <div className='text-[55px] sm:text-[45px] text-left leading-10 font-semibold sm:text-center'>
                         Mixed Pieces 
                         Earings
                     </div>
               
                 </div>
-                        <p className='text-[15px] text-[#b6acac]'>
+                        <p className='text-[1rem] text-[#b6acac]'>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam reprehenderit voluptatum, 
                             explicabo a soluta odio veniam quibusdam. Voluptatum accusantium eos voluptatibus quidem,
                             harum vel, incidunt at quibusdam repellat voluptate nihil.
                         </p>
-
-                
-              
-                
-                   
-
                 <div className='flex cursor-pointer '>
                 <h3 className='text-[18px] '>
                             EXPLORE NOW
@@ -54,14 +48,14 @@ const Home:React.FC = () => {
         </div>
 
 
-        <div className=' flex flex-col justify-center items-center '>
+        <div className=' flex flex-col justify-center items-center mt-5 '>
             <div className='text-center text-[2rem] py-3 '>
               Customer's Favorite
             </div>
 
-            <div className='flex gap-10 md:gap-5 md:flex-wrap sm:flex-wrap items-center justify-center cursor-pointer mb-5'>
-                <div className='sm:space-y-3'>
-                    <div className='h-[300px] w-[280px]'>
+            <div className='flex gap-5 md:gap-5 md:flex-wrap sm:flex-wrap items-center justify-center cursor-pointer mb-5'>
+                <div className='sm:space-y-3 w-full'>
+                    <div className='h-[300px] mb-3 sm:w-[320px]'>
                         <img className='w-[100%] h-[100%]'
                             src={D1} alt="" />
                     </div>
@@ -75,9 +69,9 @@ const Home:React.FC = () => {
                 </div>
                 
 
-                <div className='sm:space-y-3'>
-                    <div className='h-[300px] w-[280px]'>
-                        <img className='w-[100%] h-[100%]'
+                <div className='sm:space-y-3 w-full'>
+                    <div className='h-[300px] mb-3 w-full sm:w-[320px]'>
+                        <img className='w-[100%]  h-[100%]'
                             src={D3} alt="" />
                     </div>
                     <div className='text-center'>
@@ -88,8 +82,8 @@ const Home:React.FC = () => {
                     
                 </div>
                
-                <div className='sm:space-y-3'>
-                    <div className='h-[300px] w-[280px]'>
+                <div className='sm:space-y-3 w-full'>
+                    <div className='h-[300px] mb-3 w-full sm:w-[320px]'>
                         <img className='w-[100%] h-[100%]'
                             src={Earings} alt="" />
                     </div>
@@ -101,8 +95,8 @@ const Home:React.FC = () => {
                 </div>
 
 
-               <div className='sm:space-y-3'>
-                    <div className='h-[300px] w-[280px]'>
+               <div className='sm:space-y-3 w-full' >
+                    <div className='h-[300px] mb-3 sm:w-[320px]'>
                         <img className='w-[100%] h-[100%]'
                             src={D4} alt="" />
                     </div>
